395f09dd756d072f63917a0f1d427c1f4a6224d2
Markus Schanta <markus@schanta.at>
2016-04-23 19:16:27 +0100

    Offline mode for development

--- oe1.py
+++ oe1.py
@@ -6,10 +6,15 @@ BASE_URL = 'http://oe1.orf.at/programm/konsole/tag/'
 
 DEFAULT_COLUMNS = ['time', 'title', 'info']
 
-def get_oe1_program(day='20160423'):
+def get_oe1_program(day='20160423', offline=False):
+    if offline:
+        return _get_oe1_program_offline()
     url = BASE_URL + day
     response = requests.get(url, stream=True)
     return pd.DataFrame(response.json()['list'])
+
+def _get_oe1_program_offline():
+    return pd.DataFrame.from_csv('offline.csv', encoding='utf-8')
 
 def _get_date_from_row(row):
     return datetime.datetime.strptime(row['day_label'] + row['time'], '%d.%m.%Y%H:%M')
